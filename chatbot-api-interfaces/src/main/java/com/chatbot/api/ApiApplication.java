2a3c1acdd42765666ae64c2d6bd8499b4901bf79
star <a2805261291@outlook.com>
2024-07-10 18:22:55 +0800

    feat：初始化 DDD 项目

---
+++ chatbot-api-interfaces/src/main/java/com/chatbot/api/ApiApplication.java
@@ -0,0 +1,18 @@
+package com.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Fan
+ * @description 启动入口
+ * @date 2024/07/10 18:02
+ **/
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
---
+++ chatbot-api-interfaces/src/test/java/com/chatbot/api/ApiTest.java
@@ -0,0 +1,10 @@
+package com.chatbot.api;
+
+/**
+ * @author Fan
+ * @description 单元测试
+ * @date 2024/07/10 18:09
+ **/
+public class ApiTest {
+    
+}
